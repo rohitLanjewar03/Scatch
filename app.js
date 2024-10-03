a755e0e63846bab52d0baa481e94785419770cd0
rohitLanjewar03 <148527328+rohitLanjewar03@users.noreply.github.com>
2024-10-03 09:34:45 +0530

    Initial setup

---
+++ app.js
@@ -0,0 +1,2 @@
+const express = require('express');
+const userModel = require('./models/user');
---
+++ models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/scatch")
+
+const userSchema = mongoose.Schema({
+
+})
+
+module.exports = mongoose.model("user",userSchema);
